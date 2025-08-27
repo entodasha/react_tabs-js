7ab2f2210a47642361583725c6d977beac713187
Daria <darialys2510@gmail.com>
2025-08-27 17:01:34 +0200

    add fixes

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ export const tabs = [
 
 export const App = () => {
   const [activeTabId, setActiveTabId] = useState(tabs[0].id);
-  const activeTab = tabs.find(tab => tab.id === activeTabId);
+  const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
 
   return (
     <div className="section">
@@ -22,7 +22,6 @@ export const App = () => {
         <Tabs
           tabs={tabs}
           activeTabId={activeTabId}
-          activeTab={activeTab}
           onTabSelected={setActiveTabId}
         />
 
